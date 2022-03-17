c0968ded7b85bf8710786e1a89406cb05759c816
Emerson3851 <emerson.2020203851@unicap.br>
2022-03-17 15:53:47 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useState } from 'react';
+import { ImageBackground,Image, StyleSheet,TextInput, View, Button } from 'react-native';
 export default function App() {
+
+const [valor, setValor] = useState('');
+
+  const [resultado, setResultado] = useState('')
+
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require("./logo-escanor.jpg")} resizeMode = 'cover' style={styles.img1}>
+      <View>
+        <Image source={require('./escanoooor.jpg')}  resizeMode="" style={styles.img2}/>
+      </View>
+        <View style={styles.titulo}>
+        <h1>{resultado}</h1>
+          </View>       
+        <TextInput style={styles.input} placeholder="Que Personagem é esse?" onChange={(e) =>{
+    setValor(e.target.value)
+  }}/>
+        <Button title="Responder" onPress={() =>{
+    setResultado(valor)
+  }} ></Button>
+      </ImageBackground>
     </View>
   );
-}
-
+  }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#FF5733'
   },
+  img1:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent:"center",
+    width: '100&', height: '100%',
+  },
+  img2:{
+    flex:1,
+    justifyContent:"center",
+    width: 150,
+    height: 150,
+    resizeMode: 'stretch',
+    borderRadius: 500,  
+    borderRadius: 500,
+    margin: 20  
+  },
+  input: {
+    height: 40,
+    backgroundColor: '#FFF',
+    color: 'blue',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  titulo: {
+    backgroundColor: '#C18B72'
+  }
 });
